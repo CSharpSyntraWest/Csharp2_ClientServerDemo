6ed5c043e529a92c7059d398198bb02c063cf3de
Helena Copppieters <hcoppieters@hotmail.com>
2021-01-07 12:25:50 +0100

    Add project files.

---
+++ ClientApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using ServerApp;
+using System;
+using System.Collections.Generic;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello van de Client!");
+            Console.WriteLine("Opvragen van alle biernamen aan Server:");
+            DataService dataService = new DataService();
+            IList<string> biernamen = dataService.GeefAlleBiernamen();
+            foreach (string biernaam in biernamen)
+            {
+                Console.WriteLine(biernaam);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ServerApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ServerApp
+{
+    public class DataService
+    {
+        private IList<string> _biernamen;
+        public DataService()
+        {
+            _biernamen = new List<string>();
+            _biernamen.Add("Leffe Blond");
+            _biernamen.Add("Grimbergen Blond");
+            _biernamen.Add("Heineken pils");
+        }
+
+        public IList<string> GeefAlleBiernamen()
+        {
+            return _biernamen;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello van de Server!");
+            Console.ReadKey();
+        }
+    }
+}
